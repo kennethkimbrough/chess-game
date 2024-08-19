311537f89e0649fdf51520091769f9403c32b859
Kenneth Kimbrough <146112017+kennethkimbrough@users.noreply.github.com>
2024-08-18 23:15:39 -0400

    Create Instance.java

---
+++ Instance.java
@@ -0,0 +1,11 @@
+class Instance {
+    private double[] features;
+
+    public Instance(double[] features) {
+        this.features = features;
+    }
+
+    public double[] getFeatures() {
+        return features;
+    }
+}
